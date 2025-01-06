df760d443c02a3a887ef52122cd537851c232f92
= <=>
2025-01-06 12:26:14 +0300

    prototype

---
+++ binary_search.py
@@ -0,0 +1,13 @@
+from typing import List
+def binary_search(cards: List[int], query: int)-> int:
+    """_Determine the index of a given number in a list of sorted integers in descending order_
+
+    Args:
+        cards (List[int]): _List of integers in descending order_
+        query (int): _Number whose position is to be determined_
+
+    Returns:
+        int: _Position of the given number_
+    """
+    position: int = 0
+    return position
