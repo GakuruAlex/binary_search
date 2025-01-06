52f1f2977b68f9b0db1ee3702b232cddc2369cce
= <=>
2025-01-06 14:00:51 +0300

    test for number not in the list

--- test_binary_search.py
+++ test_binary_search.py
@@ -11,7 +11,9 @@ from binary_search import binary_search
     ([20, 20, 18, 18, 14, 14, 14, 12, 10, 9, 8, 5], 14, 4),
     ([20, 18, 15, 14, 13, 12, 1], 0, -1),
     ([-1, -2, -4, -6, -7, -9, -10, -12, -15, -20], -7, 4),
-    ([10, 8, 7, 6, 4, 3, 1, 0, -1, -2, -4, -5, -6], -6, 12)
+    ([10, 8, 7, 6, 4, 3, 1, 0, -1, -2, -4, -5, -6], -6, 12),
+    ([24, 16, 13, 11, 9, 8, 5, 2], 30, -1),
+    ([24, 16, 13, 11, 9, 8, 5, 2], -12, -1)
 ])
-def test_linear_search(cards, query, position):
+def test_binary_search(cards, query, position):
     assert binary_search(cards, query) == position
