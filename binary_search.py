f441b6107f8731fd033bdb83fa3d9c9a06bed85a
= <=>
2025-01-06 16:14:03 +0300

    docstr

--- binary_search.py
+++ binary_search.py
@@ -8,32 +8,57 @@ def binary_search(cards: List[int], query: int)-> int:
 
     Returns:
         int: _Position of the given number_
+    
+    Example:
+        >>> binary_search([2], 2)
+        0
+        >>> binary_search([10, 8, 7, 6, 5, 4, 2], 2)
+        6
+        >>> binary_search([10, 9, 7, 6, 5, 3, 1, 0], -1)
+        -1
     """
     low, high = 0, len(cards) -1
+    if len(cards) > 1:
+        while low <= high:
+            mid = (low + high) // 2
+            
+            result = condition(cards=cards, mid=mid, query=query)
 
-    while low <= high:
-        mid = (low + high) // 2
-        
-        result = condition(cards=cards, mid=mid, query=query)
-
-        if result == "left":
-            high = mid - 1
-        elif result == "right":
-            low = mid + 1
-        else:
-            return mid
-    return -1
+            if result == "left":
+                high = mid - 1
+            elif result == "right":
+                low = mid + 1
+            else:
+                return mid
+        return -1
+    elif len(cards) == 1:
+        return 0 if cards[0] == query else -1
+    else:
+        return -1
 def condition(cards: List[int], mid: int, query: int):
-        if cards[mid] == query and cards[mid - 1] == query:
+        """_Determine whether search space is left of the middle value , right of middle value or if target has been found._
+
+        Args:
+            cards (List[int]): _A list of sorted integers in descending order_
+            mid (int): _Middle position of the current search list_
+            query (int): _Number to determine its position_
+
+        Returns:
+            _str_: _left if query is equal to middle number and query is equal to number preceeding middle number or query is greater than middle number, right if query is less than middle number, found if number is equal_
+        """
+        middle_number = cards[mid]
+        preceeding_number = cards[mid - 1]
+        if middle_number == query and preceeding_number == query or middle_number < query:
             return 'left'
-        elif cards[mid] < query:
-            return 'left'
-        elif cards[mid] > query:
+        elif middle_number > query:
             return 'right'
         else:
             return 'found'
 def main()-> None:
-    print(binary_search([20, 18, 15, 14, 13, 12, 1], -1))
+    cards: List[int] = list(map(int, input("List of integers in descending order, separated by whitespace: ").split(" ")))
+    query: int = int(input("Number to find position: "))
+    position: int = binary_search(cards=cards, query=query)
+    print(f"{query} found at position {position} ")
 
 if __name__ == "__main__":
     main()
